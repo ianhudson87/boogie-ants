70045d95ecbda36a6677d91d24cb3a11e4b807ba
ianhudson87 <ihudson@wustl.edu>
2022-06-24 14:11:56 -0500

    bounce coroutine

--- Assets/Scripts/DanceController.cs
+++ Assets/Scripts/DanceController.cs
@@ -49,23 +49,25 @@ public class DanceController : MonoBehaviour
     IEnumerator Bounce(float bounceHeight, float bounceTime)
     {
         int numFrames = (int) (bounceTime / frameTime);
+        // Debug.Log("start bounce");
         for(int i = 0; i < numFrames; i++) {
             if(i < numFrames / 4)
                 transform.position += new Vector3(0, bounceHeight / (float)(numFrames/4), 0);
             else if (i < numFrames * 3 / 4)
-                transform.position += new Vector3(0, bounceHeight / (float)(numFrames/4), 0);
+                transform.position -= new Vector3(0, bounceHeight / (float)(numFrames/4), 0);
             else
                 transform.position += new Vector3(0, bounceHeight / (float)(numFrames/4), 0);
 
             MakeAntsCopyLeader();
             yield return new WaitForSeconds(frameTime);
         }
+        // Debug.Log("end bounce");
     }
 
     void MakeAntsCopyLeader() {
         for(int i = 0; i < ants.Length; i++) {
             ants[i].positionObject.transform.position = this.transform.position - offsets[i];
-            ants[i].positionObject.transform.eulerAngles = this.transform.eulerAngles + new Vector3(90, 0, 0);
+            ants[i].positionObject.transform.eulerAngles = this.transform.eulerAngles;
         }
     }
 }
